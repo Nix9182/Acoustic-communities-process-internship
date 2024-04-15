c77ef95dd1e0817b3583f0ade354fa6af23b76ac
Nix9182 <corentin.bodson@cri-paris.org>
2024-04-15 15:43:01 +0200

    Coded for a function to get meta info on sounds

---
+++ Temporal_distance_measures_project/temporal_distance_func_day_neigbhourhoods.R
@@ -0,0 +1,36 @@
+
+path <- "C:/Users/cobod/OneDrive/Bureau/Master BEE MNHN/Stage M1 Ecoacoustique/Données/files/"
+site <- c('BEARAV', 'GRANAM', 'MOIRAM', 'MORTCE', 'ROSSAM', 'VILOAM')
+channel <- c('left', 'right')
+
+#Function to get meta info on all sounds
+infoday = function(path, site, channel){
+  recording_w_types <- c()
+  site_list <- c()
+  channel_list <- c()
+  date_list <- c()
+  hour_list <-c()
+  sound.info <- data.frame(matrix(ncol = 0, nrow = 0))
+  for (i in site){
+    for (j in channel){
+      setwd(paste(path,"/", i, "/", j, sep=""))
+      for (k in list.files(pattern="*[0-9]$")){
+        sound.types <- read.table(k, dec=',')
+        sound.types[,3] <- formatC(sound.types[,3], digits=2,flag="0")
+        date <-strsplit(k, "_")[[1]][3]
+        hour <-substr(strsplit(k, "_")[[1]][4], 1, 2)
+        for (l in 1:dim(sound.types)[1]){
+          a <- formatC(l, digits=2,flag="0")
+          type <- sound.types[l,3]
+          type_id <- paste(type, k, a, sep='_')
+          test_list <- c(type_id, type, i, j, date, hour, a)
+          sound.info <- rbind(sound.info, test_list)
+          
+        }
+      }
+    }
+    names(sound.info) <- c("id", "type", "site", "channel", "date", "hour", "number")
+    sound.info$hour <- as.numeric(sound.info$hour)
+  }
+  return(sound.info)
+}
