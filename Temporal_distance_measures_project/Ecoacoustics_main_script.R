6124e11c4ba386d5a512238bd3faade1b13a57d1
Nix9182 <corentin.bodson@cri-paris.org>
2024-04-25 15:51:50 +0200

    Set up the main script

--- Temporal_distance_measures_project/Ecoacoustics_main_script.R
+++ Temporal_distance_measures_project/Ecoacoustics_main_script.R
@@ -2,7 +2,10 @@
 ## Set-up ----
 options(digits = 15, dplyr.summarise.inform = FALSE)
 Sys.setlocale("LC_ALL", "C")
-setwd("C:/Users/cobod/OneDrive/Bureau/Master BEE MNHN/Stage M1 Ecoacoustique/Code_and_data_Billur/lecture_1") # We will continue working under the lecture_1 directory
+setwd("C:/Users/cobod/OneDrive/Bureau/Ecoacoustic project internship/Acoustic-communities-process-internship/Temporal_distance_measures_project") 
+path <- "C:/Users/cobod/OneDrive/Bureau/Master BEE MNHN/Stage M1 Ecoacoustique/Donn�es/files/"
+site <- c('BEARAV', 'GRANAM', 'MOIRAM', 'MORTCE', 'ROSSAM', 'VILOAM')
+channel <- c('left', 'right')
 
 ## Load libraries ----
 {
@@ -23,14 +26,15 @@ setwd("C:/Users/cobod/OneDrive/Bureau/Master BEE MNHN/Stage M1 Ecoacoustique/Cod
 }
 
 ## Source functions ----
-source("SCRIPT/functions_1_prepareData.R", echo=TRUE)
-source("SCRIPT/functions_2a_runAnalysis.R", echo=TRUE)
-source("SCRIPT/functions_2b_workflow.R", echo=TRUE)
+source("C:/Users/cobod/OneDrive/Bureau/Ecoacoustic project internship/Acoustic-communities-process-internship/Temporal_distance_measures_project/Ecoac_functions_prepare_data.R", echo=TRUE)
+source("C:/Users/cobod/OneDrive/Bureau/Ecoacoustic project internship/Acoustic-communities-process-internship/Temporal_distance_measures_project/Ecoac_functions_runAnalysis.R", echo=TRUE)
+source("C:/Users/cobod/OneDrive/Bureau/Ecoacoustic project internship/Acoustic-communities-process-internship/Temporal_distance_measures_project/Ecoac_functions_workflow.R", echo=TRUE)
 
 ## Create the output folders ----
-folders = c("OUTPUTS/AlpineControl", "OUTPUTS/SubalpineControl", "OUTPUTS/AlpineWarmed")
+folders = c("OUTPUTS")
 for(i in folders){
   if(!dir.exists(i)){
     dir.create(i)
   }
 }
+
