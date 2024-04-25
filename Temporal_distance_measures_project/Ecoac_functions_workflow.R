706fddb9d9762fedeb4b0a1b45dc5879e22eaf2f
Nix9182 <corentin.bodson@cri-paris.org>
2024-04-25 16:09:15 +0200

    Started function to get differences

--- Temporal_distance_measures_project/Ecoac_functions_workflow.R
+++ Temporal_distance_measures_project/Ecoac_functions_workflow.R
@@ -32,3 +32,28 @@ no.neigh = -4:4
 
 test.Neigh = FUN1_getNeigh(Acous.releves, channel, site, date, no.neigh)
 
+#############################################################################################################
+## NO Function to get sub-neighborhoods, null communities and SES calculations for a given focal individual
+FUN3_getAcousMetrics = function(df.neigh, traits, list.acousdist, list.acouscomrand, list.acouspool)
+{
+  ## Get all null communities 
+  # randNull = fun_getRandComm(abundx = abundf, focalsp = focalsp, list.comrand = list.comrand, no.rand = no.rand, list.pool = list.pool)
+  ## Get the observed acoustic distances between focal and neighbors
+  
+  df.neigh.dist <- df.neigh
+  for(tr.dim in traits){
+    list.neigh.dist <- c()
+    for (i in 1:length(rownames(df.neigh))){
+      fst = as.character(df.neigh$fst[i])
+      nst = as.character(df.neigh$nst[i])
+      list.neigh.dist <- c(list.neigh.dist,list.acousdist[[tr.dim]][fst,nst]) 
+    }
+    df.neigh.dist <- cbind(df.neigh.dist,list.neigh.dist)
+    colnames(df.neigh.dist)[ncol(df.neigh.dist)] <- paste0("dist_", tr.dim)
+  }
+  return(df.neigh.dist)
+}
+
+###Test
+traits <- c("duration", "dom_freq")
+new.Neigh <- FUN3_getAcousMetrics(test.Neigh, traits, list.acousdist, list.acouscomrand, list.acouspool)
