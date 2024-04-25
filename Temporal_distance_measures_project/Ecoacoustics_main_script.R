734acaea4fe661c6dea00437d5a624a10b104879
Nix9182 <corentin.bodson@cri-paris.org>
2024-04-25 15:55:09 +0200

    Data setup and preprocessing

--- Temporal_distance_measures_project/Ecoacoustics_main_script.R
+++ Temporal_distance_measures_project/Ecoacoustics_main_script.R
@@ -46,3 +46,40 @@ for(i in folders){
 Acous.measures = read.csv("C:/Users/cobod/OneDrive/Bureau/Ecoacoustic project internship/Acoustic-communities-process-internship/Temporal_distance_measures_project/mesures_soundtypes_updated.csv", sep=";", dec=",")
 
 
+Acous.measures <- Acous.measures[1:(length(Acous.measures)-5)] #focus on duration and dom.frequency
+names(Acous.measures)[1] <- "sound_type"
+
+Acous.traits <- subset(Acous.measures, select = -c(id) )
+Acous.traits <- aggregate(.~sound_type, data=Acous.traits, mean) #get mean trait for each sound type
+Acous.traits$sound_type <- lapply(Acous.traits$sound_type, formatC, digits=2,flag="0")
+
+## Get soundtype n� in traits
+tr.st_names = unique(Acous.traits$sound_type)
+tr.st_no = length(tr.st_names)
+
+## Get trait names
+traits_names = c("duration", "dom_freq")
+
+## Prepare traits (log transformation + gaussianize)
+# We normalize and scale the traits to be able to compare and perform statistical tests.
+Acous.traits = Acous.traits %>%
+  mutate_at(traits_names, as.numeric)%>%
+  mutate_at(traits_names, log) %>%
+  mutate_at(traits_names, as.numeric)
+rownames(Acous.traits) = Acous.traits$sound_type
+Acous.traits = fun_gaussianizeTraits(Acous.traits, traits_names, setype = "hh")
+
+## Check trait distributions and trait space with a PCA
+tr_names = c(duration = "Duration",
+             dom_freq ="Dominant frequency")
+ggdata = melt(Acous.traits[, names(tr_names)])
+ggdata = ggdata %>% mutate(variable = recode(variable, !!!tr_names))
+ggplot(ggdata, aes(x = value)) +
+  facet_grid(.~variable, scales = "free_x") +
+  geom_histogram(alpha = 0.5) +
+  theme_bw() +
+  xlab("Log and gaussian transformed trait values") +
+  labs(fill = "Acoustic traits")
+
+res = Acous.traits[, names(tr_names)] %>% drop_na()
+corrplot(cor(res))
